fb608ff5a5d069a6d15ac5365d2f1b8cf39d7fbe
qinjunshan <13782533791@163.com>
2018-06-23 23:04:53 +0800

    首页：公司信息和导航条

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -7,11 +7,15 @@
         <Money/>
         <Product/>
         <Life/>
+        <Footor/>
+        <Navbar/>
     </div>
 </template>
 
 <script>
     import Heador from '../public/header.vue'
+    import Footor from '../public/footer.vue'
+    import Navbar from '../public/navbar.vue'
     import Slider from './hslider.vue'
     import Novice from './novice.vue'
     import Borrow from './borrow.vue'
@@ -27,7 +31,9 @@
             Borrow,
             Money,
             Product,
-            Life
+            Life,
+            Footor,
+            Navbar
         }
     }
 </script>

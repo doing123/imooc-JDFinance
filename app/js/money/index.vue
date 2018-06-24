7b8156bee9b917cad9b1ceec48321571a07d6584
qinjunshan <13782533791@163.com>
2018-06-24 22:51:08 +0800

    赚钱：广告，理财精选

--- app/js/money/index.vue
+++ app/js/money/index.vue
@@ -2,7 +2,12 @@
     <div>
         <Heador/>
         <Slider/>
-        <Footor/>
+        <Ada/>
+        <Money/>
+        <Adb/>
+        <Finance/>
+        <Adc/>
+        <Footor cname="mfooter"/>
         <Navbar/>
     </div>
 </template>
@@ -12,17 +17,34 @@
     import Footor from '../public/footer.vue'
     import Navbar from '../public/navbar.vue'
     import Slider from './mslider.vue'
+    import Ada from './advertisingA.vue'
+    import Money from './money.vue'
+    import Adb from './advertisingB.vue'
+    import Finance from './finance.vue'
+    import Adc from './advertisingC'
 
     export default {
         components: {
             Heador,
             Slider,
+            Ada,
+            Money,
+            Adb,
+            Finance,
+            Adc,
             Footor,
             Navbar
         }
     }
 </script>
 
-<style lang="scss" module>
-
+<style lang="scss">
+    .mfooter {
+        margin-top: 0 !important;
+        ul {
+            li:first-child{
+                display: none;
+            }
+        }
+    }
 </style>

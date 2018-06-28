b194ff8ec75c96f27137058119595f87af7331b3
qinjunshan <13782533791@163.com>
2018-06-28 23:17:25 +0800

    index.html模板文件 css预处理：scss-loader px2Rem  cssModule css文件提取等

--- webpackTest/app/js/home/index.vue
+++ webpackTest/app/js/home/index.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="home">
+    <div :class="$style.home">
         <h1>home</h1>
+        <div :class="$style.title">
+            title
+        </div>
     </div>
 </template>
 
@@ -10,9 +13,14 @@
     }
 </script>
 
-<style lang="css">
+<style lang="scss" module>
+    @import "../../css/reset.scss";
     .home{
         color: #f04752;
         font-size: 55px;
+        .title{
+            color: aqua;
+            font-size: 35px;
+        }
     }
 </style>
--- webpackTest/webpack.config.js
+++ webpackTest/webpack.config.js
@@ -1,46 +1,77 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
 
-module.exports = {
-    entry: {
-        app: './app/js/main.js'
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "dist"), // 静态文件在哪输出
-        compress: true, // gzip压缩
-        port: 9000
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.html$/,
-                loader: "html-loader"
-            },
-            {
-                test: /\.vue$/,
-                loader: "vue-loader"
-            },
-            {
-                test: /\.scss$/,
-                loader: "style-loader!css-loader!sass-loader"
+module.exports = env => { // 开发环境配置
+    return {
+        entry: {
+            app: './app/js/main.js'
+        },
+        devServer: {
+            contentBase: path.join(__dirname, "dist"), // 静态文件在哪输出
+            compress: true, // gzip压缩
+            port: 9000
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.html$/,
+                    loader: "html-loader"
+                },
+                {
+                    test: /\.vue$/,
+                    loader: "vue-loader",
+                    /*options: {
+                        cssModules: { // cssModule
+                            localIdentName: '[path][name]---[local]---[hash:base64:5]',
+                            camelCase: true
+                        },
+                        loaders: { // px 转 rem
+                            scss: 'vue-style-loader!css-loader!px2Rem-loader?remUnit=75&remPrecision=8!sass-loader', // <style lang="scss">
+                            css: 'vue-style-loader!css-loader!px2Rem-loader?remUnit=75&remPrecision=8'
+                        }
+                    }*/
+                    options: { // css文件 提取
+                        cssModules: { // cssModule
+                            localIdentName: '[path][name]---[local]---[hash:base64:5]',
+                            camelCase: true
+                        },
+                        loaders: { // px 转 rem
+
+                            scss: ExtractTextPlugin.extract({
+                                use: 'css-loader!px2Rem-loader?remUnit=75&remPrecision=8!sass-loader',
+                                fallback: 'vue-style-loader' // <- 这是vue-loader的依赖，所以如果使用npm3，则不需要显式安装
+                            }), // <style lang="scss">
+                            css: ExtractTextPlugin.extract({
+                                use: 'css-loader!px2Rem-loader?remUnit=75&remPrecision=8',
+                                fallback: 'vue-style-loader' // <- 这是vue-loader的依赖，所以如果使用npm3，则不需要显式安装
+                            })
+                        }
+                    }
+                },
+                {
+                    test: /\.scss$/,
+                    loader: "style-loader!css-loader!sass-loader"
+                }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(['dist']),
+            new HtmlWebpackPlugin({
+                title: 'Development',
+                template: './app/views/index.html' // 模板index文件
+            }),
+            new ExtractTextPlugin("style.css") // 提取 CSS 文件
+        ],
+        resolve: {
+            alias: { // 使用 vue 完整版
+                'vue$': 'vue/dist/vue.esm.js' // 用 webpack 1 时需用 'vue/dist/vue.common.js'
             }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            title: 'Development',
-            template: './app/views/index.html'
-        })
-    ],
-    resolve: {
-        alias: {
-            'vue$': 'vue/dist/vue.esm.js' // 用 webpack 1 时需用 'vue/dist/vue.common.js'
+        },
+        output: {
+            filename: "[name].min.js",
+            path: path.resolve(__dirname, 'dist')
         }
-    },
-    output: {
-        filename: "[name].min.js",
-        path: path.resolve(__dirname, 'dist')
     }
 }
